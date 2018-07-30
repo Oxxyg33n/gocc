4ad50fef69294f58157b840ea3fcc908c147d813
Александр Логвиненко <alexand3r.logvinenko@gmail.com>
2018-07-30 13:05:42 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,67 @@
+#GOCC - GO Currency Converter & Wrapper for OXR
+
+Wrapper & Currency Converter written in Golang for [Open Exchange Rates](https://openexchangerates.org/).
+It provides simple to use methods to make calls to the API and get responses
+
+## Installation
+Use go get:
+
+`$ go get github.com/oxxyg33n/gocc`
+
+or just clone the repository in desired location with HTTPS:
+
+`git clone https://github.com/Oxxyg33n/gocc.git`
+
+## How to use
+First, import the package:
+
+`import "github.com/oxxyg33n/gocc`
+
+Now, to show a list of all available currencies
+
+`
+		cMap, _ := gocc.AvailableCurrencies()
+`
+
+you will have a `map` of type `[string]string` that you can do anything with, for example iterate through it and
+print all the key-value pairs
+
+
+    for k := range cMap {
+        fmt.Println(k + ": " + cMap[k]) // Print out key-value pairs
+    }
+    
+Now, to show exchange rates for specific currency (available only to developer, enterprise and unlimited plans) or
+for USD (available for free).
+
+Let's say we have variable `baseCur` which is string that contains currency symbols(3-letter ISO code, USD/EUR, etc..)
+
+Then we can do something like this:
+
+
+    cMap, _ := gocc.AvailableCurrencies()   // Get list of all vailable currencies
+
+		for k := range cMap {   // Iterate through the map
+			if baseCur == k {   // If the currency specified is found then show exchange rates
+				timestamp, rates, _ := gocc.ShowExchangeRate(baseCur, false) // Timestamp is UNIX timestamp
+				
+				time := time.Unix(int64(timestamp), 0)
+				fmt.Println(time.String())
+				
+				var floatToString string
+				for rate := range rates {
+				    floatToString = strconv.FormatFloat(rates[rate], 'f', 6, 64)
+				    fmt.Println(rate + ": " + floatToString) // Print out exchange rate for currency relative to baseCur
+				}
+			} else { // If baseCur is not the code from list then continue
+				continue
+			}
+		}
+
+Now, to convert USD dollars or other base currency (available only to developer, enterprise and unlimited plans)
+to EUR/GBP/CAD/YET/BTC/LTC and print out results
+
+        convertedValue, _ := gocc.ConvertCurrency("USD", 100.50)
+        
+## Credits
+Thanks to [DiSiquera and his GoCurrency](https://github.com/DiSiqueira/GoCurrency) for request.go :)
---
+++ gocc.go
@@ -0,0 +1,128 @@
+package gocc
+
+import (
+	"encoding/json"
+	"os"
+
+	"log"
+
+	"fmt"
+
+	"strconv"
+
+	"github.com/juju/errors"
+)
+
+const baseURL = "https://openexchangerates.org/api/"
+
+type ExchangeRatesResponse struct {
+	Timestamp   int                `json:"timestamp"`
+	Rates       map[string]float64 `json:"rates"`
+	Error       bool               `json:"error"`
+	Description string             `json:"description"`
+}
+
+func (e ExchangeRatesResponse) GetTimeStamp() int {
+	return e.Timestamp
+}
+
+func (e ExchangeRatesResponse) GetRates() map[string]float64 {
+	return e.Rates
+}
+
+func (e ExchangeRatesResponse) GetError() bool {
+	return e.Error
+}
+
+func (e ExchangeRatesResponse) GetDescription() string {
+	return e.Description
+}
+
+// AvailableCurrencies() returns list of available currencies
+func AvailableCurrencies() (map[string]string, error) {
+	url := baseURL + "currencies.json"
+	resp, err := newRequest().Get(url)
+	if err != nil {
+		return nil, errors.Annotatef(err, "getting response from %s url failed", url)
+	}
+
+	cMap := map[string]string{}
+	err = json.Unmarshal(resp, &cMap)
+	if err != nil {
+		log.Println(err)
+		return nil, errors.Annotatef(err, "unmarshalling json response %s failed", resp)
+	}
+
+	return cMap, nil
+}
+
+// ShowExchangeRates(baseCurrency) returns a map with exchange rates for currency baseCurrency
+// ex: ShowExchangeRate("USD")
+func ShowExchangeRate(baseCurrency string, showAlt bool) (int, map[string]float64, error) {
+	appID := os.Getenv("OER_APP_ID")
+
+	var url string
+	if !showAlt {
+		url = fmt.Sprintf(baseURL+"latest.json?app_id=%s&base=%s", appID, baseCurrency)
+	} else {
+		url = fmt.Sprintf(baseURL+"latest.json?app_id=%s&base=%s&show_alternative=1", appID, baseCurrency)
+	}
+	resp, err := newRequest().Get(url)
+	if err != nil {
+		return 0, map[string]float64{}, errors.Annotatef(err, "getting response from %s url failed", url)
+	}
+
+	rates := ExchangeRatesResponse{}
+	err = json.Unmarshal(resp, &rates)
+	if err != nil {
+		log.Println(err)
+		return 0, map[string]float64{}, errors.Annotatef(err, "unmarshalling json response %s failed", resp)
+	}
+
+	if rates.GetError() == true {
+		return 0, nil, errors.New(rates.GetDescription())
+	}
+
+	return rates.GetTimeStamp(), rates.GetRates(), nil
+}
+
+// ConvertCurrency(amount) converts <amount> USD dollars to EURO/GBP/CAD/YEN/BTC
+// ex: ConvertCurrency(100)
+func ConvertCurrency(baseCurrency string, amount float64) (string, error) {
+	// Show alternative currencies?
+	showAlt := true
+
+	_, rates, err := ShowExchangeRate(baseCurrency, showAlt)
+	if err != nil {
+		return "", err
+	}
+
+	var currencies [6]float64
+	for k := range rates {
+		switch k {
+		case "EUR":
+			currencies[0] = amount * rates["EUR"]
+		case "GBP":
+			currencies[1] = amount * rates["GBP"]
+		case "CAD":
+			currencies[2] = amount * rates["CAD"]
+		case "JPY":
+			currencies[3] = amount * rates["JPY"]
+		case "BTC":
+			currencies[4] = amount * rates["BTC"]
+		case "LTC":
+			currencies[5] = amount * rates["LTC"]
+		}
+	}
+
+	var curString string
+	curSymbols := []string{"EUR", "GBP", "CAD", "YEN", "BTC", "LTC"}
+	for z := range curSymbols {
+		if curSymbols[z] == "LTC" && !showAlt {
+			break
+		}
+		curString += curSymbols[z] + ": " + strconv.FormatFloat(currencies[z], 'f', 6, 64) + "\n"
+	}
+
+	return curString, nil
+}
